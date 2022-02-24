69d72a8ef54d984b4ecda7ae803c13a049678932
jinpeng.he <jinpeng.he@shopee.com>
2022-02-24 15:01:15 +0800

    leetcode 32 longestValidParentheses

--- stack_queue_and_recursion/longest_valid_parentheses/longestValidParentheses.go
+++ stack_queue_and_recursion/longest_valid_parentheses/longestValidParentheses.go
@@ -1,1 +1,41 @@
 package main
+
+func longestValidParentheses(s string) int {
+	left, right, length := 0, 0, 0
+	for i := 0; i < len(s); i++ {
+		if s[i] == '(' {
+			left++
+		} else {
+			right++
+		}
+		if left < right {
+			left, right = 0, 0
+		}
+		if left == right {
+			length = max(left+right, length)
+		}
+	}
+	left, right = 0, 0
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == ')' {
+			right++
+		} else {
+			left++
+		}
+		if left > right {
+			left, right = 0, 0
+		}
+		if left == right {
+			length = max(left+right, length)
+		}
+	}
+	return length
+}
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
