b3ec756e13c5d9e0a1c6de2b6739428a5f468d07
hjp <hejinpengcs@foxmail.com>
2022-03-07 16:12:39 +0800

    moveZeroes

---
+++ array_and_linkedlist/move_zeroes/moveZeroes.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func moveZeroes(nums []int) {
+	slow, fast := 0, 0
+	for fast < len(nums) {
+		if nums[fast] != 0 {
+			nums[slow], nums[fast] = nums[fast], nums[slow]
+			slow++
+		}
+		fast++
+	}
+}
+
+func main() {
+	nums := []int{0, 1, 0, 3, 12}
+	moveZeroes(nums)
+	fmt.Println(nums)
+}
