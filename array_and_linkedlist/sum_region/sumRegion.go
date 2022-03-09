5dbbac60a3c6c8a32eb41c97474542d1e8fa7e32
hjp <hejinpengcs@foxmail.com>
2022-03-09 16:54:07 +0800

    SumRegion

---
+++ array_and_linkedlist/sum_region/sumRegion.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+type NumMatrix struct {
+	sums [][]int
+}
+
+func Constructor(matrix [][]int) NumMatrix {
+	m := len(matrix)
+	if m == 0 {
+		return NumMatrix{}
+	}
+	n := len(matrix[0])
+	sums := make([][]int, m+1)
+	sums[0] = make([]int, n+1)
+	for i, row := range matrix {
+		sums[i+1] = make([]int, n+1)
+		for j, v := range row {
+			sums[i+1][j+1] = sums[i+1][j] + sums[i][j+1] - sums[i][j] + v
+		}
+	}
+	return NumMatrix{sums}
+}
+
+func (nm *NumMatrix) SumRegion(row1, col1, row2, col2 int) int {
+	return nm.sums[row2+1][col2+1] - nm.sums[row1][col2+1] - nm.sums[row2+1][col1] + nm.sums[row1][col1]
+}
+
+func main() {
+	matrix := [][]int{
+		{3, 0, 1, 4, 2},
+		{5, 6, 3, 2, 1},
+		{1, 2, 0, 1, 5},
+		{4, 1, 0, 1, 7},
+		{1, 0, 3, 0, 5},
+	}
+	preSum := Constructor(matrix)
+	fmt.Println(preSum)
+	fmt.Println(preSum.SumRegion(2, 1, 4, 3))
+}
