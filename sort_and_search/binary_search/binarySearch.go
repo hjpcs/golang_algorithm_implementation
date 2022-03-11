0ce3aeeae81e01a42d5c5797152e6bc652b2ad8e
hjp <hejinpengcs@foxmail.com>
2022-03-11 11:15:01 +0800

    binarySearch with left index and right index

--- sort_and_search/binary_search/binarySearch.go
+++ sort_and_search/binary_search/binarySearch.go
@@ -1,4 +1,6 @@
-package binary_search
+package main
+
+import "fmt"
 
 func search(nums []int, target int) int {
 	left, right := 0, len(nums)-1
@@ -13,4 +15,53 @@ func search(nums []int, target int) int {
 		}
 	}
 	return -1
+}
+
+func searchRange(nums []int, target int) []int {
+	var res []int
+	res = append(res, left_search(nums, target))
+	res = append(res, right_search(nums, target))
+	return res
+}
+
+func left_search(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			right = mid - 1
+		} else if nums[mid] > target {
+			right = mid - 1
+		} else if nums[mid] < target {
+			left = mid + 1
+		}
+	}
+	if left >= len(nums) || nums[left] != target {
+		return -1
+	}
+	return left
+}
+
+func right_search(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			left = mid + 1
+		} else if nums[mid] > target {
+			right = mid - 1
+		} else if nums[mid] < target {
+			left = mid + 1
+		}
+	}
+	if right < 0 || nums[right] != target {
+		return -1
+	}
+	return right
+}
+
+func main() {
+	nums := []int{5, 7, 7, 7, 7, 8, 8, 10}
+	target := 8
+	fmt.Println(searchRange(nums, target))
 }
